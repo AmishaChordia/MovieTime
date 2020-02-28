fd5f37222045e31dc165236e036d131c7f1233d8
Amisha Chordia <achordia@deloitte.com>
2020-02-28 17:16:20 +0530

    Initial project setup

---
+++ MovieTime/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  MovieTime
+//
+//  Created by Chordia, Amisha on 28/02/20.
+//  Copyright © 2020 Amisha. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ MovieTime/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieTime
+//
+//  Created by Chordia, Amisha on 28/02/20.
+//  Copyright © 2020 Amisha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
